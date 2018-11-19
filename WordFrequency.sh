c4cfbad98bcbe885fa42d502ce10e227919e2694
Sagar Wani <ssswanil@gmail.com>
2018-11-19 17:27:39 -0500

    Update WordFrequency.sh

--- WordFrequency.sh
+++ WordFrequency.sh
@@ -16,5 +16,5 @@
 #
 #Don't worry about handling ties, it is guaranteed that each word's frequency count is unique.
 #Could you write it in one-line using Unix pipes?
-
+#!/bin/bash
 cat words.txt | tr [:space:] "\n" | sed '/^$/d' | tr '[:upper:]' '[:lower:]' | sort | uniq -c | awk '{print $2,$1}' | sort -k 2nr
